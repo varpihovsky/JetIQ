1b710163dc0747bd8003c02c8e8f2dfa3565c9ef
varpihovsky <pakgpcoans@gmail.com>
2021-07-05 17:56:39 +0300

    Navigation commands collecting moved to separate function.

--- app/src/main/java/com/varpihovsky/jetiq/MainActivity.kt
+++ app/src/main/java/com/varpihovsky/jetiq/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.rememberNavController
 import com.varpihovsky.jetiq.system.navigation.NavigationManager
+import com.varpihovsky.jetiq.ui.compose.CollectNavigationCommands
 import com.varpihovsky.jetiq.ui.compose.Root
 import com.varpihovsky.jetiq.ui.theme.JetIQTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,18 +27,12 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             val viewModel = hiltViewModel<SharedViewModel>()
 
-            navigationManager.commands.collectAsState().value.also {
-                if (it.destination.isNotEmpty() && it.destination != viewModel.getCurrentDestination()) {
-                    viewModel.onDestinationChange(it.destination)
-                    navController.navigate(it.destination) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
-            }
+            CollectNavigationCommands(
+                navigationManager = navigationManager,
+                currentDestination = viewModel::getCurrentDestination,
+                onDestinationChange = viewModel::onDestinationChange,
+                navController = navController
+            )
 
             JetIQTheme {
                 Root(sharedViewModel = viewModel, navController = navController)
