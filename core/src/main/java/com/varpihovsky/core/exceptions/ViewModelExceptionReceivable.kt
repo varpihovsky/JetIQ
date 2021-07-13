76b852ecdb1ea516e724abcb28e4a19b315320f1
varpihovsky <pakgpcoans@gmail.com>
2021-07-14 02:00:27 +0300

    [core] Changed exception type.

--- core/src/main/java/com/varpihovsky/core/exceptions/ViewModelExceptionReceivable.kt
+++ core/src/main/java/com/varpihovsky/core/exceptions/ViewModelExceptionReceivable.kt
@@ -3,7 +3,7 @@ package com.varpihovsky.core.exceptions
 import android.util.Log
 
 interface ViewModelExceptionReceivable : ViewModelWithException {
-    fun send(e: Exception) {
+    fun send(e: Throwable) {
         Log.d("ViewModels", Log.getStackTraceString(e))
         exceptions.value = e
     }
--- core/src/main/java/com/varpihovsky/core/exceptions/ViewModelWithException.kt
+++ core/src/main/java/com/varpihovsky/core/exceptions/ViewModelWithException.kt
@@ -3,7 +3,7 @@ package com.varpihovsky.core.exceptions
 import kotlinx.coroutines.flow.MutableStateFlow
 
 interface ViewModelWithException {
-    val exceptions: MutableStateFlow<Exception?>
+    val exceptions: MutableStateFlow<Throwable?>
 
     fun onExceptionProcessed() {
         exceptions.value = null
