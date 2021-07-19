960a333fa09d53619e5af1ad33d09732dcab693d
varpihovsky <pakgpcoans@gmail.com>
2021-07-19 06:48:53 +0300

    [core] Adapted AppbarManager to pass data through event bus.

--- core/src/main/java/com/varpihovsky/core/appbar/AppbarManager.kt
+++ core/src/main/java/com/varpihovsky/core/appbar/AppbarManager.kt
@@ -1,6 +1,14 @@
 package com.varpihovsky.core.appbar
 
-import com.varpihovsky.core.FlowManager
+import com.varpihovsky.core.eventBus.EventBus
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.mapNotNull
 import javax.inject.Inject
 
-class AppbarManager @Inject constructor() : FlowManager<AppbarCommand>(AppbarCommand.Empty)
+class AppbarManager @Inject constructor(private val eventBus: EventBus) {
+    val commands = eventBus.bus.mapNotNull { it as? AppbarCommand }.distinctUntilChanged()
+
+    fun manage(command: AppbarCommand) {
+        eventBus.push(command)
+    }
+}
