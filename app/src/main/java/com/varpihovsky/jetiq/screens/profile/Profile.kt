c6c5a1c08c9faf92a860b1935ae43de849447a1f
varpihovsky <pakgpcoans@gmail.com>
2021-07-20 11:42:26 +0300

    [app] Removed previews in Profile.kt.

--- app/src/main/java/com/varpihovsky/jetiq/screens/profile/Profile.kt
+++ app/src/main/java/com/varpihovsky/jetiq/screens/profile/Profile.kt
@@ -1,12 +1,10 @@
 package com.varpihovsky.jetiq.screens.profile
 
-import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -16,12 +14,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
@@ -30,109 +26,15 @@ import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.SwipeRefreshState
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.varpihovsky.jetiq.ui.compose.*
-import com.varpihovsky.jetiq.ui.theme.JetIQTheme
 import com.varpihovsky.ui_data.MarksInfo
 import com.varpihovsky.ui_data.UIProfileDTO
 import com.varpihovsky.ui_data.UISubjectDTO
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
-val exampleProfile = UIProfileDTO(
-    0,
-    "Подрезенко Владислав",
-    "ФКСА",
-    1,
-    "1ІСТ-20б",
-    2,
-    "https://iq.vntu.edu.ua/b06175/getfile.php?stud_id=11411"
-)
-
 const val SCROLL_DP = 503f
 const val AVATAR_SIZE = 200
 const val SCROLL_OFFSET = -177
-
-@ExperimentalAnimationApi
-@Preview
-@Composable
-fun ProfilePreviewLight() {
-    JetIQTheme(darkTheme = false) {
-        ExampleProfile()
-    }
-}
-
-@ExperimentalAnimationApi
-@Preview
-@Composable
-fun ProfilePreviewDark() {
-    JetIQTheme(darkTheme = true) {
-        ExampleProfile()
-    }
-}
-
-@ExperimentalAnimationApi
-@Composable
-private fun ExampleProfile() {
-    val successSubjectsList = mutableListOf<UISubjectDTO>()
-
-    repeat(15) { i ->
-        val semester = if (i < 7) 1 else 2
-        successSubjectsList.add(
-            UISubjectDTO(
-                0,
-                "Комп'ютерна графіка",
-                "Софина Ольга Юріївна",
-                100,
-                semester
-            )
-        )
-    }
-
-    val markbookSubjectsList = mutableListOf<UISubjectDTO>()
-
-    var semester = 1
-
-    repeat(40) { i ->
-        if (semester % 10 == 0) {
-            semester++
-        }
-
-        markbookSubjectsList.add(
-            UISubjectDTO(
-                0,
-                "Комп'ютерна графіка",
-                "Софина Ольга Юріївна",
-                100,
-                semester
-            )
-        )
-    }
-
-    val markbookInfoList = listOf(
-        MarksInfo(1, 100),
-        MarksInfo(2, 100),
-        MarksInfo(3, 100),
-        MarksInfo(4, 100)
-    )
-
-    var successChecked by remember { mutableStateOf(false) }
-    var markbookChecked by remember { mutableStateOf(false) }
-
-    Profile(
-        profile = exampleProfile,
-        scrollState = rememberScrollState(),
-        successMarksInfo = listOf(MarksInfo(1, 100), MarksInfo(2, 100)),
-        subjects = successSubjectsList,
-        markbookInfo = markbookInfoList,
-        markbookSubjects = markbookSubjectsList,
-        successChecked = successChecked,
-        onSuccessToggle = { successChecked = !successChecked },
-        markbookChecked = markbookChecked,
-        onMarkbookToggle = { markbookChecked = !markbookChecked },
-        refreshState = rememberSwipeRefreshState(isRefreshing = true),
-        onRefresh = {},
-        onSettingsClick = {}
-    )
-}
 
 @ExperimentalAnimationApi
 @Composable
@@ -195,9 +97,6 @@ fun Profile(
     val heightState = (((a / SCROLL_DP)) * 180f).dp
     val profileTextShown = heightState == 180.dp
 
-    Log.d("App", scrollState.value.toString())
-    Log.d("App", heightState.value.toString())
-
     ProfileAppBar(
         profile = profile,
         heightState = heightState,
@@ -225,7 +124,6 @@ fun Profile(
                 Success(
                     modifier = Modifier.onGloballyPositioned {
                         successPosition = scrollState.value + it.positionInRoot().y + SCROLL_OFFSET
-                        Log.d("Application", "Success: $successPosition")
                     },
                     successMarksInfo = successMarksInfo,
                     subjects = subjects,
@@ -246,7 +144,6 @@ fun Profile(
                 Markbook(
                     modifier = Modifier.onGloballyPositioned {
                         markbookPosition = scrollState.value + it.positionInRoot().y + SCROLL_OFFSET
-                        Log.d("Application", "Markbook: $markbookPosition")
                     },
                     checked = markbookChecked,
                     markbookSubjects = markbookSubjects,
@@ -326,8 +223,6 @@ fun ProfileAppBar(
     onSettingsClick: () -> Unit
 ) {
     val elevation = if (profileTextShown) 10.dp else 0.dp
-    val backgroundColor =
-        if (heightState == 180.dp) MaterialTheme.colors.primary else Color.Transparent
     val zIndex = if (heightState == 180.dp) 10f else 0f
 
     Box(
