5f983cb287b2731cecd2e4d936356bdbc4e80557
varpihovsky <pakgpcoans@gmail.com>
2021-08-15 23:25:40 +0300

    [core_nav] Changed copyright notice location.

--- core_nav/src/androidMain/kotlin/com/varpihovsky/core_nav/navigation/BottomNavigationItemFactory.kt
+++ core_nav/src/androidMain/kotlin/com/varpihovsky/core_nav/navigation/BottomNavigationItemFactory.kt
@@ -1,5 +1,3 @@
-package com.varpihovsky.core_nav.navigation
-
 /* JetIQ
  * Copyright © 2021 Vladyslav Podrezenko
  *
@@ -16,6 +14,7 @@ package com.varpihovsky.core_nav.navigation
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+package com.varpihovsky.core_nav.navigation
 
 object BottomNavigationItemFactory {
     fun create(navigationCommand: NavigationCommand) = when (navigationCommand) {
