da69752303c1117d5f108acbb4c94e3a82b64ddc
varpihovsky <pakgpcoans@gmail.com>
2021-08-27 23:13:29 +0300

    [core_ui] Added StaggeredVerticalGrid layout.

--- core_ui/src/commonMain/kotlin/com/varpihovsky/core_ui/compose/foundation/Layout.kt
+++ core_ui/src/commonMain/kotlin/com/varpihovsky/core_ui/compose/foundation/Layout.kt
@@ -22,6 +22,9 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.Dp
+import kotlin.math.ceil
 
 @Composable
 fun CenterLayout(
@@ -79,6 +82,61 @@ fun SwipeRefresh(
     content: @Composable () -> Unit
 ) {
     SwipeRefreshActual(modifier, isRefreshing, onRefresh, indicatorPadding, content)
+}
+
+@Composable
+fun StaggeredVerticalGrid(
+    modifier: Modifier = Modifier,
+    maxColumnWidth: Dp,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        content = content,
+        modifier = modifier
+    ) { measurables, constraints ->
+        check(constraints.hasBoundedWidth) {
+            "Unbounded width not supported"
+        }
+        val columns = ceil(constraints.maxWidth / maxColumnWidth.toPx()).toInt()
+        val columnWidth = constraints.maxWidth / columns
+        val itemConstraints = constraints.copy(maxWidth = columnWidth)
+        val colHeights = IntArray(columns) { 0 } // track each column's height
+        val placeables = measurables.map { measurable ->
+            val column = shortestColumn(colHeights)
+            val placeable = measurable.measure(itemConstraints)
+            colHeights[column] += placeable.height
+            placeable
+        }
+
+        val height = colHeights.maxOrNull()?.coerceIn(constraints.minHeight, constraints.maxHeight)
+            ?: constraints.minHeight
+        layout(
+            width = constraints.maxWidth,
+            height = height
+        ) {
+            val colY = IntArray(columns) { 0 }
+            placeables.forEach { placeable ->
+                val column = shortestColumn(colY)
+                placeable.place(
+                    x = columnWidth * column,
+                    y = colY[column]
+                )
+                colY[column] += placeable.height
+            }
+        }
+    }
+}
+
+private fun shortestColumn(colHeights: IntArray): Int {
+    var minHeight = Int.MAX_VALUE
+    var column = 0
+    colHeights.forEachIndexed { index, height ->
+        if (height < minHeight) {
+            minHeight = height
+            column = index
+        }
+    }
+    return column
 }
 
 @Composable
